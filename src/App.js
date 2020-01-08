9afc69af92381f45727067dc0dc9c13f59670936
Mikhail Semin <mikhail.semin@emlid.com>
2020-01-08 19:20:22 +0300

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { formatDistance } from 'date-fns';
+
+const startDate = new Date(2020, 0, 8, 2, 46);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <p>Hi I'm Mike.</p>
+            <p>I am finding the way to relocate to Berlin: <span>{
+                formatDistance(
+                    Date.now(),
+                    startDate,
+                    {
+                        addSuffix: true,
+                        includeSeconds: true
+                    }
+                )
+            }</span></p>
+
+            <p>Do you have something for me? Write me: <a href="https://twitter.com/seminioni">twitter.com/seminioni</a></p>
+            <p>Github: <a href="https://github.com/seminioni">github.com/seminioni</a></p>
+        </div>
+    );
 }
 
 export default App;
